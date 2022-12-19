3c3051bd24bdd2e83662993ca8a3a6260fd9f036
Fan Jiang <prof.fan@foxmail.com>
2022-12-19 14:28:56 -0500

    Multiplication

--- Sources/ShapedArray/ShapedArray+Differentiable.swift
+++ Sources/ShapedArray/ShapedArray+Differentiable.swift
@@ -4,6 +4,39 @@ extension ShapedArray: Differentiable where Scalar: Differentiable & AdditiveAri
     public typealias TangentVector = Self
 }
 
+extension ShapedArray where Scalar: Differentiable & FloatingPoint {
+    @differentiable(reverse, wrt: (lhs, rhs))
+    public static func * (_ lhs: Self, _ rhs: Self) -> Self {
+        var result = lhs
+        result *= rhs
+        return result
+    }
+
+    @differentiable(reverse)
+    public static func *= (_ lhs: inout Self, _ rhs: Self) {
+        if lhs._isScalarZero {
+            lhs = rhs
+        } else if !rhs._isScalarZero {
+            for i in 0..<lhs.buffer.count {
+                lhs.buffer[i] *= rhs.buffer[i]
+            }
+        }
+    }
+
+    @derivative(of:*=)
+    @usableFromInline
+    static func vjpMultiplyEquals(_ lhs: inout Self, _ rhs: Self) -> (
+        value: (), pullback: (inout Self) -> Self
+    ) {
+        lhs *= rhs
+        func pullback(_ v: inout Self.TangentVector) -> Self.TangentVector {
+            return v
+        }
+        return ((), pullback)
+    }
+
+}
+
 extension ShapedArray: AdditiveArithmetic where Scalar: Differentiable & AdditiveArithmetic {
     @differentiable(reverse)
     public static func + (_ lhs: Self, _ rhs: Self) -> Self {
